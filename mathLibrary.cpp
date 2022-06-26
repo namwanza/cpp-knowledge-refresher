055ede77dc80ff66e41553a95289216bbe5052f9
namwanza <namwanzaronald4@gmail.com>
2022-06-26 20:19:05 +0300

    working with math library

---
+++ mathLibrary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int myValue = 4;
+    // finding the square root
+    cout << "The the Square root of " << myValue << " is " << sqrt(myValue) << endl;
+    // finding the power or a number
+    cout << "In the power of 4 is " << pow(myValue, 4) << endl;
+
+    // Triagonometry
+    cout << "sin("<< myValue <<") = " << sin(myValue) << endl;
+    cout << "cos("<< myValue <<") = " << cos(myValue) << endl;
+    cout << "tan("<< myValue <<") = " << tan(myValue) << endl;
+    return 0;
+}
